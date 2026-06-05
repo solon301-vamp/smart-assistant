1859cbf7157db4d6adbbb2439e79b3b7938465c8
Nerissabila Uswatun I <hoonieiceen3011@gmail.com>
2026-06-05 17:05:26 +0700

    feat: advance backend with like, sessions, stats

--- backend/models/database.py
+++ backend/models/database.py
@@ -1,3 +1,4 @@
+#update
 from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
