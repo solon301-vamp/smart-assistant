91c9a7acae9627afe4e0e00cf56e6523cabe8bf1
Nerissabila Uswatun I <hoonieiceen3011@gmail.com>
2026-06-13 15:04:17 +0700

    fix: add google oauth client id

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+    <GoogleOAuthProvider clientId="336303479754-k5cepl9fc3rk523n0kbiufs1pna55qgt.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
